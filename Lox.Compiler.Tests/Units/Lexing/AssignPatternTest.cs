526a37a7e54734a9a7b20ba13fd34f7b7ffab7b1
Merijn Hendriks <merijn.d.hendriks@gmail.com>
2024-03-19 21:51:22 +0100

    Add unit AssignPatternTest

---
+++ Lox.Compiler.Tests/Units/Lexing/AssignPatternTest.cs
@@ -0,0 +1,37 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Lox.Compiler.Lexing;
+using Lox.Compiler.Lexing.Patterns;
+using Lox.Compiler.Tests.Common;
+using Lox.Compiler.Tests.Mocks.Lexing;
+
+namespace Lox.Compiler.Tests.Units.Lexing.Patterns
+{
+    [TestClass]
+    public sealed class AssignPatternTest
+    {
+        [TestMethod]
+        public void TestIsMatch()
+        {
+            var file = string.Empty;
+            var source = "=";
+            var pattern = new AssignPattern();
+
+            var truthResult = true;
+
+            LexingTestHelper.AssertPatternIsMatch(pattern, file, source, truthResult);
+        }
+
+        [TestMethod]
+        public void TestRun()
+        {
+            var file = string.Empty;
+            var source = "=";
+            var pattern = new AssignPattern();
+
+            var truthToken = new Token(file, 0, TokenType.ASSIGN, string.Empty);
+            var truthNext = new SourcePosition(file, 1, 1, 2);
+
+            LexingTestHelper.AssertPatternRun(pattern, file, source, truthToken, truthNext);
+        }
+    }
+}
