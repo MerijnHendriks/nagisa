3d3dc106da23eac7a4387a0bcb0fe2b339c70cd6
Merijn Hendriks <merijn.d.hendriks@gmail.com>
2024-03-20 13:13:47 +0100

    Add unit EqualPatternTest

---
+++ Lox.Compiler.Tests/Units/Lexing/Patterns/EqualPatternTest.cs
@@ -0,0 +1,37 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Lox.Compiler.Lexing;
+using Lox.Compiler.Lexing.Patterns;
+using Lox.Compiler.Tests.Common;
+using Lox.Compiler.Tests.Mocks.Lexing;
+
+namespace Lox.Compiler.Tests.Units.Lexing.Patterns
+{
+    [TestClass]
+    public sealed class EqualPatternTest
+    {
+        [TestMethod]
+        public void TestIsMatch()
+        {
+            var file = string.Empty;
+            var source = "==";
+            var pattern = new EqualPattern();
+
+            var truthResult = true;
+
+            LexingTestHelper.AssertPatternIsMatch(pattern, file, source, truthResult);
+        }
+
+        [TestMethod]
+        public void TestRun()
+        {
+            var file = string.Empty;
+            var source = "==";
+            var pattern = new EqualPattern();
+
+            var truthToken = new Token(file, 0, TokenType.EQUAL, string.Empty);
+            var truthNext = new SourcePosition(file, 2, 1, 3);
+
+            LexingTestHelper.AssertPatternRun(pattern, file, source, truthToken, truthNext);
+        }
+    }
+}
