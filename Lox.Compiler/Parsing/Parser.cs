5ef04df0e7d4d1c189a7073870dc6ffcd4f9d900
Merijn Hendriks <merijnhendriks@users.noreply.github.com>
2025-04-14 11:42:05 +0200

    Split input data from parser

--- Lox.Compiler/Parsing/Parser.cs
+++ Lox.Compiler/Parsing/Parser.cs
@@ -7,9 +7,8 @@ namespace Lox.Compiler.Parsing
 {
     public sealed class Parser
     {
-        private int _current;
-        private ScanResult _scanResult;
         private readonly Logger _logger;
+        private ParserData _parserData;
 
         public Parser(Logger logger)
         {
@@ -18,62 +17,26 @@ namespace Lox.Compiler.Parsing
 
         public Expr Run(ScanResult scanResult)
         {
-            this._current = 0;
-            this._scanResult = scanResult;
+            this._parserData = new ParserData(scanResult);
 
             return this.Expression();
         }
-
-/*
-        private void Synchronize()
-        {
-            this.Advance();
-
-            while (!this.IsAtEnd())
-            {
-                if (this.Previous().Type == TokenType.SEMICOLON)
-                {
-                    // end of line
-                    return;
-                }
-
-                switch (this.Peek().Type)
-                {
-                    case TokenType.CLASS:
-                    case TokenType.FUN:
-                    case TokenType.VAR:
-                    case TokenType.FOR:
-                    case TokenType.IF:
-                    case TokenType.WHILE:
-                    case TokenType.PRINT:
-                    case TokenType.RETURN:
-                        return;
-
-                    default:
-                        break;
-                }
-
-                this.Advance();
-            }
-        }
-*/
 
         private Expr Expression()
         {
             return this.Equality();
         }
 
-        // EQUALITY
-        // a != b
-        // a == b
         private Expr Equality()
         {
             Expr expr = this.Comparison();
 
-            while (this.Match(TokenType.EQUAL)
-                || this.Match(TokenType.NOT_EQUAL))
+            // a != b
+            // a == b
+            while (this._parserData.Match(TokenType.EQUAL)
+                || this._parserData.Match(TokenType.NOT_EQUAL))
             {
-                Token op = this.Previous();
+                Token op = this._parserData.Previous();
                 Expr right = this.Comparison();
                 expr = new Binary(expr, op, right);
             }
@@ -81,21 +44,20 @@ namespace Lox.Compiler.Parsing
             return expr;
         }
 
-        // COMPARISON
-        // a <= b
-        // a < b
-        // a >= b
-        // a > b
         private Expr Comparison()
         {
             Expr expr = this.Term();
 
-            while (this.Match(TokenType.GREATER_EQUAL)
-                || this.Match(TokenType.RIGHT_ARROW)
-                || this.Match(TokenType.LESS_EQUAL)
-                || this.Match(TokenType.LEFT_ARROW))
+            // a <= b
+            // a < b
+            // a >= b
+            // a > b
+            while (this._parserData.Match(TokenType.GREATER_EQUAL)
+                || this._parserData.Match(TokenType.RIGHT_ARROW)
+                || this._parserData.Match(TokenType.LESS_EQUAL)
+                || this._parserData.Match(TokenType.LEFT_ARROW))
             {
-                Token op = this.Previous();
+                Token op = this._parserData.Previous();
                 Expr right = this.Term();
                 expr = new Binary(expr, op, right);
             }
@@ -103,17 +65,16 @@ namespace Lox.Compiler.Parsing
             return expr;
         }
 
-        // TERM:
-        // a - b
-        // a + b
         private Expr Term()
         {
             Expr expr = this.Factor();
 
-            while (this.Match(TokenType.MINUS)
-                || this.Match(TokenType.PLUS))
+            // a - b
+            // a + b
+            while (this._parserData.Match(TokenType.MINUS)
+                || this._parserData.Match(TokenType.PLUS))
             {
-                Token op = this.Previous();
+                Token op = this._parserData.Previous();
                 Expr right = this.Factor();
                 expr = new Binary(expr, op, right);
             }
@@ -121,17 +82,16 @@ namespace Lox.Compiler.Parsing
             return expr;
         }
 
-        // FACTOR:
-        // a / b
-        // a * b
         private Expr Factor()
         {
             Expr expr = this.Unary();
 
-            while (this.Match(TokenType.SLASH)
-                || this.Match(TokenType.STAR))
+            // a / b
+            // a * b
+            while (this._parserData.Match(TokenType.SLASH)
+                || this._parserData.Match(TokenType.STAR))
             {
-                Token op = this.Previous();
+                Token op = this._parserData.Previous();
                 Expr right = this.Unary();
                 expr = new Binary(expr, op, right);
             }
@@ -139,15 +99,14 @@ namespace Lox.Compiler.Parsing
             return expr;
         }
 
-        // UNARY:
-        // -10
-        // !10
         private Expr Unary()
         {
-            if (this.Match(TokenType.NOT)
-             || this.Match(TokenType.MINUS))
+            // -10
+            // !10
+            if (this._parserData.Match(TokenType.NOT)
+             || this._parserData.Match(TokenType.MINUS))
             {
-                Token op = this.Previous();
+                Token op = this._parserData.Previous();
                 Expr right = this.Unary();
                 return new Unary(op, right);
             }
@@ -155,120 +114,45 @@ namespace Lox.Compiler.Parsing
             return this.Primary();
         }
 
-        // PRIMARY:
-        // false
-        // true
-        // nil
-        // 10
-        // text
-        // ( )
         private Expr Primary()
         {
-            if (this.Match(TokenType.FALSE))
+            // false
+            if (this._parserData.Match(TokenType.FALSE))
             {
                 return new Literal(false);
             }
 
-            if (this.Match(TokenType.TRUE))
+            // true
+            if (this._parserData.Match(TokenType.TRUE))
             {
                 return new Literal(true);
             }
 
-            if (this.Match(TokenType.NIL))
+            // nil
+            if (this._parserData.Match(TokenType.NIL))
             {
                 return new Literal(null);
             }
 
-            if (this.Match(TokenType.NUMBER)
-             || this.Match(TokenType.STRING))
+            // 10
+            // text
+            if (this._parserData.Match(TokenType.NUMBER)
+             || this._parserData.Match(TokenType.STRING))
             {
-                return new Literal(this.Previous().Value);
+                return new Literal(this._parserData.Previous().Value);
             }
 
-            if (this.Match(TokenType.LEFT_CIRCLE))
+            // ( )
+            if (this._parserData.Match(TokenType.LEFT_CIRCLE))
             {
                 Expr expr = this.Expression();
-                this.Consume(TokenType.RIGHT_CIRCLE, "Expect ')' after expression.");
+                this._parserData.Consume(TokenType.RIGHT_CIRCLE, "Expect ')' after expression.");
                 return new Grouping(expr);
             }
 
-            throw ExpressionError();
-        }
-
-        private InvalidOperationException ExpressionError()
-        {
-            SourcePosition position = this._scanResult.Sourcemap[this._current];
-            string format = "[{0}]: Expected expression for token {1} at [idx:{2},ln:{3},col:{4}].";
-            string message = string.Format(
-                format,
-                position.File,
-                this.Peek().Type,
-                position.Index,
-                position.Line,
-                position.Column);
-
-            return new InvalidOperationException(message);
-        }
-
-        private bool Match(int type)
-        {
-            if (this.Check(type))
-            {
-                this.Advance();
-                return true;
-            }
-
-            return false;
-        }
-
-        private Token Consume(int type, string message)
-        {
-            if (this.Check(type))
-            {
-                return this.Advance();
-            }
-
-            throw new InvalidOperationException(this.Peek() + message);
-        }
-
-        private Token Advance()
-        {
-            if (!this.IsAtEnd())
-            {
-                ++this._current;
-            }
-
-            return this.Previous();
-        }
-
-        private bool Check(int type)
-        {
-            if (this.IsAtEnd())
-            {
-                return false;
-            }
-
-            return this.Peek().Type == type;
-        }
-
-        private bool IsAtEnd()
-        {
-            if (this.Peek().Type == TokenType.END_OF_FILE)
-            {
-                return true;
-            }
-
-            return false;
-        }
-
-        private Token Peek() 
-        {
-            return this._scanResult.Tokens[this._current];
-        }
-
-        private Token Previous()
-        {
-            return this._scanResult.Tokens[this._current - 1];
+            // No matching expression found
+            Exception ex = this._parserData.ExpressionError();
+            throw ex;
         }
     }
 }
